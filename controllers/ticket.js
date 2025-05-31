9f7c38a4cfb6e106fffffa94846fc55b82bed77b
Khush Shah <khushshah675@gmail.com>
2025-05-31 13:45:10 +0530

    ticket controller completed.

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -7,64 +7,76 @@ export const createTicket = async (req, res) => {
     if (!title || !description) {
       return res
         .status(400)
-        .json({ error: "Title and description are required" });
+        .json({ message: "Title and description are required" });
     }
-    const ticket = await Ticket.create({
+    const newTicket = Ticket.create({
       title,
       description,
-      createdBy: req.user._id,
+      createdBy: req.user._id.toString(),
     });
+
     await inngest.send({
       name: "ticket/created",
       data: {
-        ticketId: ticket._id,
-        title: ticket.title,
-        description: ticket.description,
-        createdBy: ticket.createdBy,
+        ticketId: (await newTicket)._id.toString(),
+        title,
+        description,
+        createdBy: req.user._id.toString(),
       },
     });
     return res.status(201).json({
-      success: true,
-      message: "Ticket created successfully",
-      ticket,
+      message: "Ticket created and processing started",
+      ticket: newTicket,
     });
-  } catch (err) {
-    console.error("error creating ticket", err);
-    return res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: err.message,
-    });
+  } catch (error) {
+    console.error("Error creating ticket", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
 export const getTickets = async (req, res) => {
   try {
     const user = req.user;
+    let tickets = [];
     if (user.role !== "user") {
-      const tickets = await Ticket.find()
+      tickets = Ticket.find({})
         .populate("assignedTo", ["email", "_id"])
         .sort({ createdAt: -1 });
-      return res.status(200).json({
-        success: true,
-        message: "Tickets fetched successfully",
-        tickets,
-      });
     } else {
-      const tickets = await Ticket.find({ createdBy: user._id })
-        .select("title description createdAt status")
+      tickets = await Ticket.find({ createdBy: user._id })
+        .select("title description status createdAt")
         .sort({ createdAt: -1 });
-      return res.status(200).json({
-        success: true,
-        message: "Tickets fetched successfully",
-        tickets,
-      });
     }
-  } catch (err) {
-    console.error("error fetching tickets", err);
-    return res.status(500).json({
-      success: false,
-      message: "Internal server error",
-      error: err.message,
-    });
+    return res.status(200).json(tickets);
+  } catch (error) {
+    console.error("Error fetching tickets", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getTicket = async (req, res) => {
+  try {
+    const user = req.user;
+    let ticket;
+
+    if (user.role !== "user") {
+      ticket = Ticket.findById(req.params.id).populate("assignedTo", [
+        "email",
+        "_id",
+      ]);
+    } else {
+      ticket = Ticket.findOne({
+        createdBy: user._id,
+        _id: req.params.id,
+      }).select("title description status createdAt");
+    }
+
+    if (!ticket) {
+      return res.status(404).json({ message: "Ticket not found" });
+    }
+    return res.status(404).json({ ticket });
+  } catch (error) {
+    console.error("Error fetching ticket", error.message);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
